367d3badd32d6c01c55cb53fdd047651158fda53
Edson Jr <eddson.jr@gmail.com>
2017-08-30 21:15:00 -0400

    Initial Commit

---
+++ textSpeech_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textSpeech_iOS
+//
+//  Created by Edson  Jr on 30/08/17.
+//  Copyright © 2017 Edson  Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
